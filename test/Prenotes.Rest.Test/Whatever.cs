d33c7acfcc4a30c379386fcfc4df0da1a1a62ff2
Matthew Young <matthewdyoung@gmail.com>
2017-01-01 15:35:08 +0100

    Skeleton with xunit in the test project

---
+++ test/Prenotes.Rest.Test/Whatever.cs
@@ -0,0 +1,24 @@
+using Xunit;
+
+namespace Prenotes.Rest.Test
+{
+    public class Whatever
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
